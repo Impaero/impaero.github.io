4de7f821e2ce72a76a8688d1cb9cd3657edccf59
Krishna Aguinaldo <S3942619@student.rmit.edu.au>
2025-04-10 18:42:50 +1000

    RW database initialised

---
+++ sea-manifestation/script.js
@@ -0,0 +1,14 @@
+const landingPage1 = `
+<div class="warning">
+<img src="../home-assets/images/dragon-solid-white.png" height="50px" width="60px">
+<p>
+THIS DATABASE IS ONLY FOR THE RIFT WATCH’S EYES ONLY.<br>
+IF UNAUTHORISED, TURN BACK. ONLY INSANITY AWAITS ON YOU.
+</p>
+<button id="warning-button">wawa</button>
+</div>
+`;
+
+const body = document.querySelector("#body");
+
+body.innerHTML = landingPage1;
