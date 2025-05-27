d1389ae94c37c29455d3ba472887cd6b6f5a80da
Krishna Aguinaldo <S3942619@student.rmit.edu.au>
2025-05-27 23:50:49 +1000

    test stuff so i can chuck it to my other laptop for testing

--- sea-manifestation/script.js
+++ sea-manifestation/script.js
@@ -1,3 +1,7 @@
+// Some global variables with null in order to initialise future editing with the pages.
+let showcase = undefined;
+
+// Landing Page - Warning
 const landingPage1 = `
 <div class="warning">
 <img src="../home-assets/images/dragon-solid-white.png" height="50px" width="60px">
@@ -9,18 +13,32 @@ IF UNAUTHORISED, TURN BACK. ONLY INSANITY AWAITS ON YOU.
 </div>
 `;
 
-const landingPage2 = `<div class="homepage">
-    <div class="page-header">
-        
-    </div>
-</div>`;
-let landingPage2Exist = false;
+
+const landingPage2 = `
+    <div id="main-page">
+            <div class="header-container">
+                <div class="rift-watch-icon">
+                    <img src="../home-assets/images/dragon-solid-white.png" height="60px" width="60px" float="left">   
+                </div>
+                <div class="rift-watch-top-text">
+                    <h1>RIFT WATCH DATABASE</h1>
+                </div>
+            </div>
+            <div class="main-body">
+                <div class="sidebar">
+                    <button class="sidebar-b" onclick="sidebarFunction('wawa')">TESTING BUTTON</button>
+                </div>
+                <div class="showcase-container" id="showcase">
+                    <p>testing text</p>
+                </div>
+        </div>
+    </div>`;
 
 let body = document.querySelector("#body");
 
 body.innerHTML = landingPage1;
 
-// sets warning page so that 
+// sets warning page so that the user does not need to click on it everytime they refresh the page.
 function setWarningTrue() {
     document.cookie = "warning=true;";
 }
@@ -29,15 +47,26 @@ function checkWarningTrue() {
 
 }
 
+// swap from warning page to main page
 function warningReceived() {
-    showDatabase(0); 
+    body.innerHTML = landingPage2;
+    showcase = document.querySelector("#showcase");
+    // return true;
 }
 
-function showDatabase(id) {
+function sidebarFunction(functionName) {
+    if (functionName == "wawa") {
+        showDatabase(0, showcase);
+    }
+}
+
+function showDatabase(id, showcase) {
     const xmlhttp = new XMLHttpRequest();
     xmlhttp.onload = function () {
         const db = JSON.parse(this.responseText);
-        body.innerHTML = db.manifestDatabase[id].description.replaceAll("\n","<br />");
+        showcase.innerHTML = `
+        <p>${db.manifestDatabase[id].description.replaceAll("\n","<br />")}</p>
+        `;
     };
     xmlhttp.open("GET", "database.json");
     xmlhttp.send();
